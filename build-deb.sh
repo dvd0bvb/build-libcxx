fee3e159fb11abe71068a5f4534e261ed8dd1147
David Brill <6david6brill6@gmail.com>
2024-08-11 21:57:33 -0400

    Changed install directory for package binaries

--- build-deb.sh
+++ build-deb.sh
@@ -1,10 +1,9 @@
 # copy binaries and headers
-mkdir -p libc++-with-msan/usr
-mkdir -p libc++-with-msan/usr/include
-mkdir -p libc++-with-msan/usr/lib
+mkdir -p libc++-with-msan/usr/include/libcxx-msan
+mkdir -p libc++-with-msan/usr/lib/libcxx-msan
 
-cp -r llvm-project/build/include/* libc++-with-msan/usr/include/
-cp -r llvm-project/build/lib/* libc++-with-msan/usr/lib/
+cp -r llvm-project/build/include/* libc++-with-msan/usr/include/libcxx-msan
+cp -r llvm-project/build/lib/* libc++-with-msan/usr/lib/libcxx-msan
 
 # build the package
 dpkg-deb --build libc++-with-msan
