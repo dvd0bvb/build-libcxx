15cef083d56e8fd35ae01900a3f47bd8577f364c
David Brill <6david6brill6@gmail.com>
2025-09-28 20:04:09 -0400

    Remove unnecessary files

--- build-deb.sh
+++
@@ -1,9 +0,0 @@
-# copy binaries and headers
-mkdir -p libc++-with-msan/usr/include/libcxx-msan
-mkdir -p libc++-with-msan/usr/lib/libcxx-msan
-
-cp -r llvm-project/build/include/* libc++-with-msan/usr/include/libcxx-msan
-cp -r llvm-project/build/lib/* libc++-with-msan/usr/lib/libcxx-msan
-
-# build the package
-dpkg-deb --build libc++-with-msan
